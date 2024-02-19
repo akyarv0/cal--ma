2b39c11144fc021715a87ecd2ee565c6ab693a73
akyarv0 <akyarv0@gmail.com>
2024-02-19 23:08:17 +0300

    çalışma soruları

---
+++ code-class/teamwork.js
@@ -0,0 +1,19 @@
+// "Sıralı bir dizi içinde (örneğin, bir dizi içinde) tekrar eden sayıları bulun ve bu tekrar eden sayıların her biri için kaç kere tekrar ettiğini hesaplayın. Sonuçları bir obje olarak döndürün."
+
+
+
+
+
+// "Sıralı bir dizi içindeki her bir sayının, dizideki tüm önceki sayılarla çarpımını hesaplayın. Örneğin, [1, 2, 3, 4] dizisi için sonuç [1, 2, 6, 24] olmalıdır."
+
+
+
+
+
+
+// "Verilen bir dizideki her bir kelimenin uzunluğunu hesaplayarak bu uzunlukları içeren yeni bir dizi oluşturun." Örneğin ["merhaba", "dünya", "javascript"] sonucu [7, 5, 10] olamlı
+
+
+
+
+// "Verilen bir dizideki pozitif çift sayıların karelerini alarak bu sayıları sıralı bir şekilde büyükten küçüğe doğru listeleyin."
