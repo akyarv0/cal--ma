91d1a25965226cf541495cec6fffd83c6220f4a4
akyarv0 <akyarv0@gmail.com>
2024-02-20 21:43:02 +0300

    new

--- code-class/teamwork.js
+++ code-class/teamwork.js
@@ -2,10 +2,10 @@
 
 
 
-
-
-const arr1 = [12, 15, 2, 4, 12, 2, 7, 8,4]
-const tekrar = arr1.filter((item,index,array)=> array.indexOf(item)!== index)
+const arr1 = [12, 15, 2, 4, 12, 2, 7, 8, 4];
+const tekrar = arr1.filter(
+  (item, index, array) => array.indexOf(item) !== index
+);
 console.log(tekrar);
 
 
@@ -14,16 +14,14 @@ console.log(tekrar);
 
 // "Sıralı bir dizi içindeki her bir sayının, dizideki tüm önceki sayılarla çarpımını hesaplayın. Örneğin, [1, 2, 3, 4] dizisi için sonuç [1, 2, 6, 24] olmalıdır."
 
-const sayilar=[1, 2, 3, 4]
+const sayilar = [1, 2, 3, 4];
 
-for (let i=1; i<sayilar.length;i++){
-    sayilar[i]=sayilar[i]*sayilar[i-1]
-
+for (let i = 1; i < sayilar.length; i++) {
+  sayilar[i] = sayilar[i] * sayilar[i - 1];
 }
 console.log(sayilar);
 
 // "Verilen bir dizideki her bir kelimenin uzunluğunu hesaplayarak bu uzunlukları içeren yeni bir dizi oluşturun." Örneğin ["merhaba", "dünya", "javascript"] sonucu [7, 5, 10] olamlı
-
 
 const words = ["merhaba", "dünya", "javascript"];
 const lenghts = [];
@@ -32,29 +30,20 @@ for (let i in words) {
 }
 console.log(lenghts);
 
-
 // ipucu --->const wordsNum = words[0].length // 7
 
-
 // "Verilen bir dizideki pozitif çift sayıların karelerini alarak bu sayıları sıralı bir şekilde büyükten küçüğe doğru listeleyin."
-const arr2 = [12, 15, 2, 4, 12, 2, 7, 8,4]
-const arr2Evens=[];
+
+const arr2 = [12, 15, 2, 4, 12, 2, 7, 8, 4];
+const arr2Evens = [];
 for (let i of arr2) {
-    if(i%2===0 && i>0){
-        i=i**2
-        arr2Evens.push(i)
-    }
-
+  if (i % 2 === 0 && i > 0) {
+    i = i ** 2;
+    arr2Evens.push(i);
+  }
 }
-console.log(arr2Evens);
-console.log( arr2Evens.sort((a,b)=>b-a));
 
-
-
-
-
-
-
+console.log(arr2Evens.sort((a, b) => b - a));
 
 
 
@@ -67,4 +56,3 @@ console.log( arr2Evens.sort((a,b)=>b-a));
 // let cohort = myArr[2];
 // cohort.toLowerCase();
 // console.log(cohort);
-
