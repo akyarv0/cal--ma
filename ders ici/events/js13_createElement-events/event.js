4edef1e0840e3a4563f3c255924f2fa8e8eadf21
akyarv0 <akyarv0@gmail.com>
2024-02-23 22:52:06 +0300

    ders içi

--- ders ici/events/js13_createElement-events/event.js
+++ ders ici/events/js13_createElement-events/event.js
@@ -11,8 +11,11 @@ yazdir();
 //? Selectorler
 const h2 = document.querySelector(".add-item h2");
 const body = document.querySelector("body");
-
 const myInput = document.getElementById("input");
+
+const add = document.getElementById("btn");
+
+const ul = document.querySelector("ul");
 
 //?event tanımlamalari
 
@@ -24,8 +27,17 @@ h2.onmouseover = function () {
 
 //* mouse h2 elemenitinin üzerinden ayrıldığınıda tetiklenir
 
-
-
 body.onload = function () {
-  console.log("body yüklendi");
-}
+  myInput.focus();
+};
+
+add.onclick = function (e) {
+  // console.log(e);
+  // console.log(e.target);
+  const li = document.createElement("li");
+  li.textContent = myInput.value;
+  console.log(li);
+  ul.appendChild(li);
+  myInput.value = "";
+};
+
