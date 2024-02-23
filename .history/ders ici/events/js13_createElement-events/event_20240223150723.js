5a5dfd59772982d3f14b62fb79d266806f743ae5
akyarv0 <akyarv0@gmail.com>
2024-02-23 15:20:26 +0300

    event

--- .history/ders ici/events/js13_createElement-events/event_20240223150723.js
+++ .history/ders ici/events/js13_createElement-events/event_20240223150723.js
@@ -10,9 +10,6 @@ yazdir();
 
 //? Selectorler
 const h2 = document.querySelector(".add-item h2");
-const body = document.querySelector("body");
-
-const myInput = document.getElementById("input");
 
 //?event tanımlamalari
 
@@ -23,9 +20,3 @@ h2.onmouseover = function () {
 };
 
 //* mouse h2 elemenitinin üzerinden ayrıldığınıda tetiklenir
-
-
-
-body.onload = function () {
-  console.log("body yüklendi");
-}
