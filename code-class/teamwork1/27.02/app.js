ddd80f1135b33a96507d3a7acdbe96c1235cb85b
akyarv0 <akyarv0@gmail.com>
2024-02-26 22:31:56 +0300

    checkout project

---
+++ code-class/teamwork1/27.02/app.js
@@ -0,0 +1,90 @@
+//! Bir restoranın menüsünü temsil eden bir JavaScript nesnesi oluşturun. Bu nesne, menüdeki yemeklerin adlarını özellikler olarak içermelidir. Ayrıca, her yemeğin fiyatını döndüren bir metodu da içermelidir. Oluşturduğunuz bu nesneyi kullanarak, menüdeki bir yemeğin adını almak ve fiyatını öğrenmek için nasıl bir JavaScript kodu yazarsınız?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//! Bir şirketin çalışanlarını temsil eden bir JavaScript uygulaması yapmanız isteniyor. Her çalışanın adı, departmanı ve maaşı bilgilerini içeren bir nesne dizisi oluşturun. Ardından, şirket içindeki departmanlara göre çalışanların maaş ortalamasını hesaplayan bir fonksiyon yazın. Bu fonksiyon, departman adını parametre olarak alacak ve ilgili departmandaki çalışanların maaş ortalamasını döndürecek. Eğer belirtilen departmanda çalışan yoksa, "Belirtilen departmanda çalışan bulunamadı!" mesajını döndürün. Örneğin:
+
+var calisanlar = [
+    { ad: "Ali", departman: "Muhasebe", maas: 4000 },
+    { ad: "Ayşe", departman: "İnsan Kaynakları", maas: 6000 },
+    { ad: "Fatma", departman: "Muhasebe", maas: 4500 },
+    { ad: "Mehmet", departman: "Satış", maas: 5500 },
+    { ad: "Ahmet", departman: "İnsan Kaynakları", maas: 5000 }
+  ];
+  
+  function departmanMaasOrtalamasi(departman) {
+    // Kodunuzu buraya yazın
+  }
+  
+  // Örnek kullanım:
+  console.log(departmanMaasOrtalamasi("Muhasebe")); // Beklenen çıktı: 4250
+  console.log(departmanMaasOrtalamasi("Satış")); // Beklenen çıktı: 5500
+  console.log(departmanMaasOrtalamasi("Bilgi Teknolojileri")); // Beklenen çıktı: Belirtilen departmanda çalışan bulunamadı!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  //! Bir online öğrenci takip sistemi geliştiriyorsunuz. Her öğrencinin adı, sınıfı ve aldığı dersler bilgilerini içeren bir öğrenci nesnesi oluşturun. Ayrıca, her dersin adı, öğretmeni ve öğrencilere verilen notlar bilgilerini içeren bir ders nesnesi oluşturun.
+
+// 1. Öğrencilerin derslere kaydolmasını sağlayan bir fonksiyon yazın. Bu fonksiyon, öğrencinin adını ve kaydolmak istediği dersleri almalıdır.
+// 2. Bir öğrencinin aldığı notları görüntüleyen bir fonksiyon yazın. Bu fonksiyon, öğrencinin adını almalı ve aldığı dersler ile bu derslerde aldığı notları listelemelidir.
+// 3. Bir dersin öğrencilerinin not ortalamasını hesaplayan bir fonksiyon yazın. Bu fonksiyon, dersin adını almalı ve bu derse kayıtlı öğrencilerin not ortalamasını döndürmelidir.
+
+// Bu özellikleri gerçekleştiren bir JavaScript programı yazın.
+
+
+// Öğrenci ve ders nesneleri oluşturulması
